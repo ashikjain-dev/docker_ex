5ce57f6c14a52c650bed8ff3483a39d6d7aa0d2d
ashik-jain <ashikjain33@gmail.com>
2026-01-23 16:14:14 +0530

    feat:get articles route

--- routes/article.js
+++ routes/article.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const { authenticate } = require("../middlewares");
 const { validateArticle } = require("../middlewares/validate");
 const { getDB } = require("../config/mongo");
 const { logger } = require("../logger");
+const { decrypt } = require("../utils/encryptdecrypt");
 const articleRoutes = express.Router();
 articleRoutes.post(
   "/create",
@@ -13,7 +15,7 @@ articleRoutes.post(
       const { title, description, tags } = req.body;
       const db = await getDB();
       const payload = {
-        userId: req.id,
+        userId: new ObjectId(req.id),
         title,
         description,
         tags,
@@ -37,4 +39,63 @@ articleRoutes.post(
     }
   },
 );
+articleRoutes.get("/", async (req, res, next) => {
+  try {
+    let { page, limit } = req.query;
+    if (!page) {
+      page = 1;
+    }
+    if (!limit) {
+      limit = 10;
+    }
+    limit = Number(limit) > 30 ? 10 : Number(limit);
+    const offset = Number(page) - 1;
+    const db = await getDB();
+    const resFromDB = await db
+      .collection("articles")
+      .aggregate([
+        { $skip: offset },
+        { $limit: limit },
+        {
+          $lookup: {
+            from: "users",
+            localField: "userId",
+            foreignField: "_id",
+            as: "author_details",
+          },
+        },
+        {
+          $project: {
+            title: 1,
+            description: 1,
+            tags: 1,
+            createdAt: 1,
+            "author_details.firstName": 1,
+            _id: 0,
+          },
+        },
+        {
+          $unwind: {
+            path: "$author_details",
+          },
+        },
+      ])
+      .toArray();
+    for (let i = 0; i < resFromDB.length; i++) {
+      resFromDB[i].author_details.firstName = decrypt(
+        resFromDB[i].author_details.firstName,
+      );
+    }
+    res.status(200).json({ data: resFromDB });
+  } catch (error) {
+    res.status(500).json({ data: "something went wrong" });
+    logger.error("server error", {
+      statusCode: res.statusCode,
+      error: error.message,
+      stack: error.stack,
+      method: req.method,
+      url: req.originalUrl,
+    });
+  }
+});
 module.exports = { articleRoutes };
